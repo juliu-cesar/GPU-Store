36bf826733ee056c0c5d2c78c6625134f4847b71
Juliu Cesar <juliucesarcardoso@gmail.com>
2023-03-23 20:30:40 -0300

    adding board

--- src/ProductPage/components/ProductDescription/components/StyledProductDescription.ts
+++ src/ProductPage/components/ProductDescription/components/StyledProductDescription.ts
@@ -14,8 +14,8 @@ export const StyledProductDescription = styled.div`
     flex-direction: row;
     align-items: center;
     gap: 15px;
-    .text_description{
-      padding: 10px;      
+    .text_description {
+      padding: 10px;
     }
     h3 {
       font-size: 19px;
@@ -36,9 +36,8 @@ export const StyledProductDescription = styled.div`
       border-radius: 10px;
     }
   }
-  .reverse{
+  .reverse {
     flex-direction: row-reverse;
-    
   }
   .center_description {
     width: 100%;
@@ -48,13 +47,47 @@ export const StyledProductDescription = styled.div`
     border-top: 2px solid;
     border-bottom: 2px solid;
     border-color: ${({ theme }) => theme.text_color};
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+    h3 {
+      font-size: 19px;
+      font-weight: 500;
+      margin-bottom: 10px;
+    }
+    h4 {
+      font-size: 17px;
+      font-weight: 600;
+      margin-bottom: 15px;
+    }
+    p {
+      font-size: 14px;
+      font-weight: 400;
+    }
     img {
       width: 100%;
       border-radius: 10px;
     }
-  }  
+  }
   @media (max-width: 770px) {
     .description_card {
+      h3 {
+        font-size: 17px;
+        font-weight: 500;
+        margin-bottom: 10px;
+      }
+      h4 {
+        font-size: 15px;
+        font-weight: 600;
+        margin-bottom: 15px;
+      }
+      p {
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 15px;
+      }
+    }
+    .center_description {
       h3 {
         font-size: 17px;
         font-weight: 500;
@@ -74,13 +107,13 @@ export const StyledProductDescription = styled.div`
   }
   @media (max-width: 670px) {
     .description_card {
-    width: 95%;
-    padding: 10px;
-    margin: auto;
-    flex-direction: column;
-    img{
-      width: 90%;
-    }
+      width: 95%;
+      padding: 10px;
+      margin: auto;
+      flex-direction: column;
+      img {
+        width: 90%;
+      }
     }
   }
 `;
