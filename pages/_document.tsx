dbfd1e440b6bffb58016e1418e2649825fad9d28
Juliu Cesar <juliucesarcardoso@gmail.com>
2023-04-14 14:53:40 -0300

    changing styledComponents to SSR

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,62 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render(): any {
     return (
-        <Html lang="pt-br">
-            <Head />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
-                rel="stylesheet"
-            />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Genos:wght@300;400;500;600&display=swap"
-                rel="stylesheet"
-            />
-            <body style={{"backgroundColor": "#000"}}>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
+      <Html lang="pt-br">
+        <Head />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Genos:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+        <body style={{ backgroundColor: "#000" }}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
     );
+  }
 }
